a79572009b0de4ba16cb2c70e06ac88600c3cdd0
snehal <snehalreddy@gmail.com>
2025-04-21 21:41:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Sidebar />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Todoinput.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todoinput = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Todoinput
